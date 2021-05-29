1c4f3b2c842e520b18fbcce601acdc1643e0dc5a
Superdadccs57 <crazytrain401@hotmail.com>
2021-05-28 20:29:18 -0600

    Updated commit

--- jQuery_project/Project/js/script.js
+++ jQuery_project/Project/js/script.js
@@ -6,18 +6,20 @@ $(document).ready(function() {
 	console.log('script loaded successfully')
 	
 	
-	$('.btn-random').on('click', function() {
+	$('.btn-random').on('click', function(e) {
 	
+		e.preventDefault()
+		
 		const $randomHero = Math.ceil(Math.random()*731);
 		const $inputText = $('input:text');
 		
-		$.get(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${$randomHero}.json`, function(data) {
+		$.get(`https://superheroapi.com/api.php/10165589462060227/${$randomHero}/`, function(data) {
 			
 			$("#name").text(data['name']);
-			$("img").attr("src", data['images']["sm"]);
+			$("img").attr("src", data['image']['url']);
 			
-		$('.btn-guess').on('click', function() {
-			
+		$('.btn-guess').on('click', function(e) {
+			e.preventDefault()
 			console.log(data['biography']['publisher']);
 			if ($inputText.val() == data['biography']['publisher']) {
 				alert('You are correct!')
